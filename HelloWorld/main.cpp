5af8c4773b113d9923b33700469ed924c2c2342c
mathp0455 <mathp0455@EDS-IC0026495.wrdsb.ca>
2017-04-25 10:20:29 -0400

    Entering source code

---
+++ HelloWorld/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: mathp0455
+ *
+ * Created on April 25, 2017, 10:19 AM
+ */
+
+#include <cstdlib>
+#include <iostream>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"Hello World!";
+    return 0;
+}
+
